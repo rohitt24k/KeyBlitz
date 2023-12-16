daaaed32dc0a8917a7d2b81c2f58ea3bff918c00
rohitt24k <rohitt24k@gmail.com>
2023-12-16 23:05:24 +0530

    refactor: link change

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://keyblitz.vercel.app/",
+    origin: "https://keyblitz.vercel.app",
     credentials: true,
   })
 );
